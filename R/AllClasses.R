23ee80a42574e9a97d1dc0215e6e8f564c6b95ac
mikejiang <wjiang2@fhcrc.org>
2013-01-28 16:46:25 -0800

    add new gatingTemplate class and method

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -4,41 +4,138 @@
 ###############################################################################
 ## exsits for the purpose of dispatching 
 setClass("gatingTemplate",
-		contains="data.frame"
+		contains="graphNEL"
 		,representation(name="character")
-		,validity=function(object){
-			#check if the parent is ambiguous or not valid			
-			parent_matches<-sapply(levels(object[,"parent"]),function(x){
-						matches<-length(grep(x,object[,"pop"],fix=T))
-						if(matches==0&&x=="root")
-							matches<-1
-						matches
-					})
-			
-			invalid_parent<-which(parent_matches==0)
-			ambiguous_parent<-which(parent_matches>1)
-			if(length(invalid_parent)>0)
-				return(paste("The following parent population names are not valid:"
-							,paste(names(invalid_parent)
-								,collapse=",")
-							)
-						)
-			else if(length(ambiguous_parent)>0)
-				return(paste("The following parent population names are ambiguous:"
-								,paste(names(ambiguous_parent)
-										,collapse=",")
-						)
-				)
-			else 
-				TRUE
-		}
+#		,validity=function(object){
+#			#check if the parent is ambiguous or not valid			
+#			parent_matches<-sapply(levels(object[,"parent"]),function(x){
+#						matches<-length(grep(x,object[,"pop"],fix=T))
+#						if(matches==0&&x=="root")
+#							matches<-1
+#						matches
+#					})
+#			
+#			invalid_parent<-which(parent_matches==0)
+#			ambiguous_parent<-which(parent_matches>1)
+#			if(length(invalid_parent)>0)
+#				return(paste("The following parent population names are not valid:"
+#							,paste(names(invalid_parent)
+#								,collapse=",")
+#							)
+#						)
+#			else if(length(ambiguous_parent)>0)
+#				return(paste("The following parent population names are ambiguous:"
+#								,paste(names(ambiguous_parent)
+#										,collapse=",")
+#						)
+#				)
+#			else 
+#				TRUE
+#		}
 	)
 
-
+setClass("gatingMethod"
+		,representation(name="character"
+						,dims="character"
+						,args="character"
+						)	
+)
 
 gatingTemplate<-function(file,name){
-	gt<-read.csv(file)
-	gt<-as(gt,"gatingTemplate")
+	#read csv
+	df<-read.csv(file,as.is=T)
+	#create graph with root node
+	g<-graphNEL(nodes="0",edgemode="directed")
+	nodeDataDefaults(g,"label")<-"root"
+	edgeDataDefaults(g,"gatingMethod")<-""
+	#parse each row
+	nEdges<-nrow(df)
+	edgs<-vector("list",nEdges)
+	for(i in 1:nEdges){
+		
+		curNodeID<-as.character(i)
+		parent<-df[i,"parent"]
+		curPop<-df[i,"alias"]
+#		browser()
+		gm<-new("gatingMethod"
+				,name=df[i,"method"]
+				,dims=df[i,"dims"]
+				,args=df[i,"args"]
+				)
+		cat("Adding population:",curPop,"\n")
+		if(parent=="root")
+		{
+			src<-"0"
+			g<-graph::addNode(curNodeID,g)
+			nodeData(g,curNodeID,"label")<-curPop
+			g<-addEdge(src,curNodeID,g)
+#			browser()
+			edgeData(g,src,curNodeID,"gatingMethod")<-gm
+		}else
+		{
+			#travese the graph to get node list for matching later on
+			discovered<-dfs(g)[[1]]
+			g<-graph::addNode(curNodeID,g)
+			nodeData(g,curNodeID,"label")<-curPop
+			#split by logical operator in case of boolean gates
+			refNodes<-strsplit(parent,"&|\\|")[[1]]
+			for(refNode in refNodes)
+			{
+						#split by "/" for each reference node
+						
+#				browser()
+				tokens<-strsplit(refNode,"/")[[1]]
+				##locate the first token in traversed node list 
+				firstToken<-tokens[1]
+				tokens<-tokens[-1]
+				curAncesterID<-NULL
+				for(ancesterID in discovered)
+				{
+#										browser()
+					if(nodeData(g,ancesterID,"label")==firstToken)
+					{
+						curAncesterID<-ancesterID
+						
+					}
+					
+				}
+				if(is.null(curAncesterID))
+					stop("Population '",firstToken,"' not found")
+				#start from matchedID to match the rest of tokens in the path
+				while(length(tokens)>0)
+				{
+					curToken<-tokens[1]
+					tokens<-tokens[-1]
+					#find the id within the edges sourced from current ancester
+					dests<-edges(g,curAncesterID)[[1]]
+					curAncesterID<-NULL
+					for(ancesterID in dests)
+					{
+#										browser()
+						if(nodeData(g,ancesterID,"label")==curToken)
+						{
+							curAncesterID<-ancesterID
+							
+						}
+						
+					}
+					if(is.null(curAncesterID))
+						stop("Population '",curToken,"' not found")
+				}
+#				browser()
+				g<-addEdge(curAncesterID,curNodeID,g)
+				edgeData(g,curAncesterID,curNodeID,"gatingMethod")<-gm
+			}
+			
+		}
+	
+	}
+	
+	
+	
+#	browser()
+	gt<-as(g,"gatingTemplate")
 	gt@name<-name
 	gt
 }
+
--- R/gatingTemplate-accessors.R
+++ R/gatingTemplate-accessors.R
@@ -21,7 +21,7 @@ setMethod("gating", signature = c("gatingTemplate", "GatingSet"), definition = f
 						thisCall<-substitute(
 										get(gm)(x=x
 												,wf=gs
-												,pViewName=parent
+												,parent=parent
 												,name=pop
 												,xChannel = dims[1]
 												, yChannel = dims[2]
@@ -51,25 +51,68 @@ setMethod("show",signature=c("gatingTemplate"),definition=function(object)
 
 			cat("--- Gating Template: ")
 			cat(object@name)
-#			cat("\ntransformations:")
-#			print(getTransformations(object))
-#			cat("\ncompensations:")
-#			print(getCompensationMatrices(object))
-#			cat(" ---")
 			cat("\n")
-#			cat("\nquadrantGate",fill=T,labels="TH,NL:")
-#			cat(tsubMarkers(object),fill=T,labels="Markers:",sep=",")
-#			cat("\n")
+#			browser()
+			cat("\twith ",length(object@nodes)," populations\n");
 		})
-#setMethod("getTransformations",signature=c("gatingTemplate"),definition=function(x)
-#		{
-##			browser()
-#			x@transformation
-#		})
-#setMethod("getCompensationMatrices",signature=c("gatingTemplate"),definition=function(x)
-#		{
-#			
-#			x@compensation
-#		})
+setMethod("plot",signature=c("gatingTemplate"),definition=function(x,y=missing)
+		{
+			x<-as(x,"graphNEL")
+			browser()
+			
+			nAttrs<-list(label=unlist(nodeData(x,attr="label")))
+			#get gating method name attr from edges
+			gm<-unlist(lapply(edgeData(x,attr="gatingMethod"),names))
+			eCols<-gm
+			#fix the name attr 
+			e.colnames<-gsub("\\|","~",names(eCols))
+			#get the categorical method names
+			gm.txt<-unique(eCols)
+			nMethods<-length(gm.txt)
+			#encode the method name with color
+			gm.col<-RColorBrewer::brewer.pal(nMethods,name="Dark2")
+			names(gm.col)<-gm.txt
+			eCols<-gm.col[eCols]
+			names(eCols)<-e.colnames#restore names 
+			eAttrs<-list(color=eCols)
+#			browser()
+			lay<-Rgraphviz::layoutGraph(x
+					,nodeAttrs=nAttrs
+					,edgeAttrs=eAttrs
+					,attrs=list(graph=list(rankdir="LR",page=c(8.5,11))
+											,node=list(fixedsize=FALSE
+														,fillcolor="gray"
+#														,color="white"
+														,shape="ellipse"
+														)
+							
+#								,edge=list(labelfontsize="27")
+							
+								)
+			)
+			
+			
+			plot.space = par()[["usr"]]
+			x1 = plot.space[1]
+			x2 = plot.space[2]
+			y1 = plot.space[3]
+			y2 = plot.space[4]
+			
+			
+			renderGraph(lay)
+			legend(x1,y2
+					,legend=gm.txt
+					,title="Gating Methods"
+					,col=gm.col
+#					,pch="l"
+					,lty=1
+					,cex=0.8
+					)
+		})
+setMethod("names",signature=c("gatingMethod"),definition=function(x)
+		{
+			
+			x@name
+		})
 
 
