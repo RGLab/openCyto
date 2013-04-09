769b8eb83f2e67de0fceecc7a159f40a18f087c3
John Ramey <johnramey@gmail.com>
2013-04-08 23:13:05 -0700

    Tidied R source using formatR:::tidy.source.

--- R/fcFilter-methods.R
+++ R/fcFilter-methods.R
@@ -1,50 +1,39 @@
-setMethod("show",sig=c("fcFilter"),definition=function(object){
-			cat("fcFilter:")
-			callNextMethod(object)
-			
-		})
+setMethod("show", sig = c("fcFilter"), definition = function(object) {
+  cat("fcFilter:")
+  callNextMethod(object)
+})
 
-setGeneric("posteriors", function(x,y,...) standardGeneric("posteriors"))
-setMethod("posteriors",sig=c("fcFilter","ANY"),definition=function(x,y="missing"){
-			x@posteriors
-			
-		})
-setMethod("posteriors",sig=c("fcFilter","character"),definition=function(x,y){
-			post<-posteriors(x)
-			
-					
-			postNames<-names(post)
-#			if(is.null(y))
-#			{
-#				if(length(postNames)==1)
-#					post[[1]]
-#				else
-#					stop("Need to specify which prior:",paste(postNames,collapse="or"))
-#			}else
-#			{
-				ind<-match(y,postNames)
-				if(is.na(ind))
-					stop("FcFilter not found for:",y)
-				else
-					post[[ind]]		
-#			}
-			
-		})
-setGeneric("priors", function(x,y,...) standardGeneric("priors"))
-setMethod("priors",sig=c("fcFilter","ANY"),definition=function(x,y="missing"){
-      x@priors
-      
-    })
-setMethod("priors",sig=c("fcFilter","character"),definition=function(x,y){
-      prior<-priors(x)
-      
-      
-      priorNames<-names(prior)
-      ind<-match(y,priorNames)
-      if(is.na(ind))
-        stop("FcFilter not found for:",y)
-      else
-        prior[[ind]]		
-#			}
-      
-    })    
+setGeneric("posteriors", function(x, y, ...) standardGeneric("posteriors"))
+setMethod("posteriors", sig = c("fcFilter", "ANY"), definition = function(x, y = "missing") {
+  x@posteriors
+})
+
+setMethod("posteriors", sig = c("fcFilter", "character"),
+          definition = function(x, y) {
+  post <- posteriors(x)
+  
+  postNames <- names(post)
+  ind <- match(y, postNames)
+  if (is.na(ind)) {
+    stop("FcFilter not found for:", y)
+  } else {
+    post[[ind]]
+  }
+})
+
+setGeneric("priors", function(x, y, ...) standardGeneric("priors"))
+setMethod("priors", sig = c("fcFilter", "ANY"), definition = function(x, y = "missing") {
+  x@priors
+})
+
+setMethod("priors", sig = c("fcFilter", "character"), definition = function(x, y) {
+  prior <- priors(x)
+  
+  priorNames <- names(prior)
+  ind <- match(y, priorNames)
+  if (is.na(ind)) {
+    stop("FcFilter not found for:", y)
+  } else {
+    prior[[ind]]
+  }
+}) 
