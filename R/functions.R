f4d76fc9f55da7926c15f2dbc7e4b572ebeaaf64
mikejiang <wjiang2@fhcrc.org>
2013-09-20 13:16:28 -0700

    fix check errors

--- R/functions.R
+++ R/functions.R
@@ -445,24 +445,25 @@ channels2markers <- function(flow_frame, channels) {
 #' @param channels \code{character} specifying which channel to operate on
 #' @return a \code{flowFrame} object
 #' @examples
-#' library(flowClust)
-#' data(rituximab)
-#' # Consider the range of values for FSC.H and SSC.H
-#' summary(rituximab)
+#' \dontrun{
+#'  library(flowClust)
+#'  data(rituximab)
+#'  # Consider the range of values for FSC.H and SSC.H
+#'  summary(rituximab)
 #' 
-#' # Truncates any observations with FSC.H outside [100, 950]
-#' rituximab2 <- truncate_flowframe(rituximab, channels = "FSC.H", min = 100,
-#' max = 950)
-#' summary(rituximab2)
-#' # Next, truncates any observations with SSC.H outside [50, 1000]
-#' rituximab3 <- truncate_flowframe(rituximab2, channels = "SSC.H", min = 50,
-#' max = 1000)
-#' summary(rituximab3)
+#'  # Truncates any observations with FSC.H outside [100, 950]
+#'  rituximab2 <- truncate_flowframe(rituximab, channels = "FSC.H", min = 100, max = 950)
+#'  summary(rituximab2)
+#'  # Next, truncates any observations with SSC.H outside [50, 1000]
+#'  rituximab3 <- truncate_flowframe(rituximab2, channels = "SSC.H", min = 50, max = 1000)
+#'  summary(rituximab3)
 #'
-#' # Instead, truncates both channels at the same time
-#' rituximab4 <- truncate_flowframe(rituximab, channels = c("FSC.H", "SSC.H"),
-#' min = c(100, 50), max = c(950, 1000))
-#' summary(rituximab4)
+#'  # Instead, truncates both channels at the same time
+#'  rituximab4 <- truncate_flowframe(rituximab, channels = c("FSC.H", "SSC.H"),
+#'  min = c(100, 50), max = c(950, 1000))
+#'  summary(rituximab4)
+#' }
+#' 
 truncate_flowframe <- function(flow_frame, channels, min = NULL, max = NULL) {
   channels <- as.character(channels)
   num_channels <- length(channels)
