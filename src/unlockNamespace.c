da5b9be6fc6865d78ffcd8f606986629919f4fca
Kevin Ushey <kevinushey@gmail.com>
2013-12-03 13:04:00 -0800

    remove dependency on Rcpp; ignore src/*.so
    - we don’t need to depend on Rcpp for just one function, wherein we
    don’t even use any of the Rcpp classes.
    
    - also add ‘flowWorkspaceData’ to the Suggests: section of DESCRIPTION,
    for local vignette building

--- src/unlockNamespace.c
+++ src/unlockNamespace.c
@@ -1,18 +1,15 @@
-#include <Rcpp.h>
 #include <R.h>
 #include <Rdefines.h>
 #include <R_ext/Error.h>
-using namespace Rcpp;
 
 /* This is taken from envir.c in the R 2.15.1 source 
    https://github.com/SurajGupta/r-source/blob/master/src/main/envir.c
 */
+
 #define FRAME_LOCK_MASK (1<<14)
 #define FRAME_IS_LOCKED(e) (ENVFLAGS(e) & FRAME_LOCK_MASK)
 #define UNLOCK_FRAME(e) SET_ENVFLAGS(e, ENVFLAGS(e) & (~ FRAME_LOCK_MASK))
 
-
-//[[Rcpp::export]]
 SEXP unlockNamespace( SEXP env ) {
 
   if (TYPEOF(env) == NILSXP)
@@ -28,7 +25,5 @@ SEXP unlockNamespace( SEXP env ) {
   UNPROTECT(1);
  
   return result;
-
-  Rf_warning("your C program does not return anything!");
-  return R_NilValue;
+  
 }
