7cfcad2972af72f723a309016e68e296bede55e8
mikejiang <wjiang2@fhcrc.org>
2015-02-06 16:32:24 -0800

    1.generate mindensity or tailgate for rectangelGate. 2. generate flowClust for 2d eclipseGate

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,9 @@
 {
   options("openCyto" = list(gating = list(minEvents = 0 # minimum events needed in order for gating to proceed
                                           )
+                            , default.methods = list(gating1D = NULL #default is mindensity,tailgate,flowClust.1d
+                                                     , gating2D = NULL# default is flowClust.2d
+                                                     , singlet = NULL) # default is singletGate                                 
                             )
           )
 }
