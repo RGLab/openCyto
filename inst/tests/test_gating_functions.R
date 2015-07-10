50051528780be40b20337490a6e16496eb42634f
mikejiang <wjiang2@fhcrc.org>
2015-07-10 16:43:30 -0700

    add test suite for mindensity2 function

---
+++ inst/tests/test_gating_functions.R
@@ -0,0 +1,12 @@
+context("gating functions")
+data(GvHD)
+fr <- GvHD[[1]]
+fr <- transform(fr, transformList(c("FL1-H"),  log))
+test_that("mindensity2", {
+      
+      expect_equal(mindensity2(fr, "SSC-H"), rectangleGate(`SSC-H` = c(901.120, Inf), filterId = ""), tol = 5e-07)
+      expect_equal(mindensity2(fr, "FL1-H"), rectangleGate(`FL1-H` = c(5.216064, Inf), filterId = ""), tol = 5e-07)
+      expect_equal(mindensity2(fr, "FL2-H"), rectangleGate(`FL2-H` = c(511.0989417, Inf), filterId = ""), tol = 5e-07)
+      
+      
+    })
