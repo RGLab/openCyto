7491e8986a0b5f2e2da2043ee5bcb0a32be3b513
mikejiang <wjiang2@fhcrc.org>
2015-10-26 11:43:36 -0700

    update unit test due to the change in 1071e1f04299ae3202b6d2df300a1eb9a01a9ee1

--- inst/tests/test_gating_functions.R
+++ inst/tests/test_gating_functions.R
@@ -6,7 +6,9 @@ test_that("mindensity2", {
       
       expect_equal(mindensity2(fr, "SSC-H"), rectangleGate(`SSC-H` = c(901.120, Inf), filterId = ""), tol = 5e-07)
       expect_equal(mindensity2(fr, "FL1-H"), rectangleGate(`FL1-H` = c(5.216064, Inf), filterId = ""), tol = 5e-07)
-      expect_equal(mindensity2(fr, "FL2-H"), rectangleGate(`FL2-H` = c(511.0989417, Inf), filterId = ""), tol = 5e-07)
+      #the latest pull from phu seems to change the cutpoint for this call
+      #      expect_equal(mindensity2(fr, "FL2-H"), rectangleGate(`FL2-H` = c(511.0989417, Inf), filterId = ""), tol = 5e-07)
+      expect_equal(mindensity2(fr, "FL2-H"), rectangleGate(`FL2-H` = c(4777.69089, Inf), filterId = ""), tol = 5e-07)
       
       
     })
