59e12d90eedfffdbdd71c638682a094b1be6a2fa
mikejiang <wjiang2@fhcrc.org>
2019-06-11 16:30:21 -0700

    load flowCore

--- tests/testthat/helper-expectRes.R
+++ tests/testthat/helper-expectRes.R
@@ -1,7 +1,8 @@
 library(data.table)
 library(utils)
 library(tools)
-
+library(flowCore)
+library(ncdfFlow)
 gtFile <- system.file("extdata/gating_template/tcell.csv", package = "openCyto")
 expectResults <- readRDS("expectResults.rds")
 
