8c445188f6fbe5450c4276ecc38c42d0d0e333fb
mikejiang <mike@ozette.ai>
2021-08-05 16:27:02 -0700

    write robust_m_estimator to replace MASS::huber

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -12,3 +12,22 @@ test_that("fast collapsing for ncdfFlowSet", {
       expect_equal(fr@exprs[, -9], fr1@exprs, tol = 2e-7)
       
     })
+
+test_that("robust mean", {
+  data(GvHD)
+  fr <- GvHD[[1]]
+  x <- exprs(fr)[,1]
+  sd <- mad(x)
+  res <- robust_m_estimator(x, sd)
+  
+  expect_equal(sd, 148.26, tol = 2e-7)
+  expect_equal(res, 229.625, tol = 2e-7)
+  
+  set.seed(1)
+  x = rnorm(100)
+  sd <- mad(x)
+  res <- robust_m_estimator(x, sd)
+  expect_equal(sd, 0.8700003, tol = 2e-7)
+  expect_equal(res, 0.1153234, tol = 2e-7)
+  
+})
