1d886b9ca273611e81fa694f29569ef6ef73c1c7
mikejiang <mike@ozette.ai>
2022-07-02 12:09:11 -0700

    fix test

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,6 @@ library(openCyto)
 
 test_check("openCyto")
 
-devtools::test()
 
 #taking quite some time , thus only for internal testing
 #test_file("~/rglab/workspace/openCyto/tests/testthat/gating-testSuite.R")
