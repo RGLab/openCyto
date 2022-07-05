3c5d498e7efd4d4def1a04d2684e1617e43dae21
mikejiang <mike@ozette.ai>
2022-07-05 12:45:20 -0700

    fix start/stop.at argument in gt_gating function

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -12,3 +12,30 @@ test_that("fast collapsing for ncdfFlowSet", {
       expect_equal(fr@exprs[, -9], fr1@exprs, tol = 2e-7)
       
     })
+
+test_that("start/stop.at argument in gt_gating function", {
+  library(openCyto)
+  
+  dataDir <- system.file("extdata",package="flowWorkspaceData")
+  files <- list.files(dataDir, pattern = ".fcs$",full = TRUE)
+  fs <- read.flowSet(files[1])
+  
+  compMat <- spillover(fs[[1]]) %>% compensation
+  fs_comp <- compensate(fs, compMat)
+  chnls <- parameters(compMat)
+  transFuncts <- estimateLogicle(fs[[1]], channels = chnls)
+  fs_trans <- transform(fs_comp, transFuncts)
+  
+  gs <- GatingSet(fs_trans)
+  gt <- gatingTemplate(system.file("extdata/gating_template/testGatingTemplate.csv", package = "openCyto"))
+  gt_gating(gt, gs, stop.at = "/boundary/singlet/Alexa700_gate")
+  expect_setequal(gs_get_pop_paths(gs),c("root", "/boundary", "/boundary/singlet", "/boundary/singlet/PerCPCY5_5_gate", "/boundary/singlet/APC_gate", "/boundary/singlet/AmCyan_gate"))  
+  gt_gating(gt, gs, start = "/boundary/singlet/Alexa700_gate")
+  expect_setequal(gs_get_pop_paths(gs),c("root", "/boundary", "/boundary/singlet"
+                                        , "/boundary/singlet/PerCPCY5_5_gate"
+                                        , "/boundary/singlet/APC_gate"
+                                        , "/boundary/singlet/AmCyan_gate"
+                                        , "/boundary/singlet/Lymph"
+                                        , "/boundary/singlet/Alexa700_gate"
+                                        , "/boundary/singlet/Lymph/Alexa700pPerCPCY5_5p", "/boundary/singletRefGate", "/boundary/singletRefGate/singletRefGate_LymphRefGate"))  
+})
