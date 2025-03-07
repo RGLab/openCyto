791def1fe2730440ded83bba25fd61606a194a3c
Andrew McDavid <andrew@ozette.com>
2025-03-07 12:51:21 -0800

    update rlm tests

--- tests/testthat/test-rlm.R
+++ tests/testthat/test-rlm.R
@@ -12,12 +12,11 @@ test_that("fast_rlm", {
   # r1 <- MASS::rlm(y~x, df)
   system.time(r1 <- MASS::rlm(x, y))
   system.time(r2 <- fast_rlm(x, y))
-  expect_equal(r1$coefficients, r2$coefficients)
+  # r2$coefficients is unnamed
+  expect_equal(r1$coefficients, r2$coefficients, check.attributes = FALSE)
+  
   #del unrelevant parameters before comparison
-  r1[["psi"]] <- NULL
-  r1[["conv"]] <- NULL
-  r1[["call"]] <- NULL
-  r1[["k2"]] <- NULL
+  r1 = r1[names(r2)]
   expect_equivalent(r1, r2)
   # library(ggplot2)
   # ggplot(df, aes(x,y)) +geom_point() + geom_abline(slope = r1$coefficients)
